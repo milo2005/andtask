a46288d8de4f2faee28de7eff560307abd199163
Dario Chamorro <dario.chamorro.v@gmail.com>
2019-11-16 16:15:20 -0500

    Adapter implemented

--- app/src/main/java/com/example/tareas/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/tareas/ui/adapter/TaskAdapter.kt
@@ -1,2 +1,46 @@
 package com.example.tareas.ui.adapter
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tareas.R
+import com.example.tareas.data.db.model.Task
+import com.example.tareas.databinding.TemplateTaskBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class TaskAdapter: RecyclerView.Adapter<TaskHolder>(){
+
+    var data:List<Task> =  emptyList()
+        set(value){
+            field = value
+            notifyDataSetChanged()
+        }
+
+    private val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.template_task, parent, false)
+        return TaskHolder(view)
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: TaskHolder, position: Int) =
+        holder.bind(data[position],dateFormat)
+
+
+}
+
+class TaskHolder(view:View):RecyclerView.ViewHolder(view){
+
+    private val binding:TemplateTaskBinding = TemplateTaskBinding.bind(view)
+
+    fun bind(task: Task,format:SimpleDateFormat ){
+        binding.task = task
+        binding.dateFormat = format
+    }
+
+}
--- app/src/main/java/com/example/tareas/util/Attr.kt
+++ app/src/main/java/com/example/tareas/util/Attr.kt
@@ -1,5 +1,6 @@
 package com.example.tareas.util
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import java.text.SimpleDateFormat
@@ -8,4 +9,9 @@ import java.util.*
 @BindingAdapter("app:dateValue", "app:dateFormat")
 fun dateToString(txt:TextView,date:Date, dateFormat:SimpleDateFormat){
     txt.text = dateFormat.format(date)
+}
+
+@BindingAdapter("app:imgRes")
+fun imgFromResource(img:ImageView, res:Int){
+    //
 }
